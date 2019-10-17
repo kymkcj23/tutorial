6f21193772392a0d58b0f4dfde390a1fa97dbcd3
tutorial <kymkcj23@naver.com>
2019-10-17 23:10:44 +0900

    Modified templates to display posts from database.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,10 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from . import views                           # !!!
 
 urlpatterns = [
+    path('', views.homepage, name='home'),    # !!!
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
+    # path('', include('blog.urls')),
 ]
